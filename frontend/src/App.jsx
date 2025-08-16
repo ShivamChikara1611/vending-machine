0b20244b160896942c3c52f93ba89a97f4bcba89
shivam chikara <s.chikara6885@gmail.com>
2025-08-16 16:26:22 +0530

    initialize the navbar and home file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import { Routes, Route } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <ToastContainer />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   )
 }
 
